03febeed28f6fcb3ef953d20091a29368f3daf02
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-06-18 20:09:01 -0500

    Add files via upload

---
+++ sweetAlert.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: 'Devols',
+    html: 'Adquiere la nueva clave con <b><a class="devolstext" href="https://t.me/Peru_Pro">@Peru_Pro</a></b>',
+    footer: '<a class="comprardevols" href="https://t.me/Peru_Pro">Compra aquí</a>',
+})
